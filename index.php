885249b1a11716bb7bd9bf9b5555c49aa9dd3d9c
muskanatwork <kumari.muskan2302@gmail.com>
2026-04-26 16:03:03 +0530

    Created task display feature

--- index.php
+++ index.php
@@ -17,6 +17,20 @@
             <input type="text" name="task" placeholder="Enter Task" required>
             <button type="submit">Add Task</button>
         </form>
+
+        <ul>
+            <?php
+            $query = mysqli_query($conn, "SELECT * FROM tasks ORDER BY id DESC");
+
+            while ($row = mysqli_fetch_assoc($query)) {
+            ?>
+                <li>
+                    <?php echo $row['task']; ?>
+                </li>
+            <?php } ?>
+        </ul>
+
+
     </div>
 
 </body>
