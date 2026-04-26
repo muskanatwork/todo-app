e30861ae0a69a72564a0d8175df755ba2f3b039d
muskanatwork <kumari.muskan2302@gmail.com>
2026-04-26 12:19:57 +0530

    Added database connection

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<h1>Muskan</h1>
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Todo App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Todo List</h1>
+
+        <form action="add.php" method="POST">
+            <input type="text" name="task" placeholder="Enter Task" required>
+            <button type="submit">Add Task</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
